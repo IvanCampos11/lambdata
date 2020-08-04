1ba47a2f9224a17a8c81a14c08ddc89d9256c268
IvanCampos11 <39417756+IvanCampos11@users.noreply.github.com>
2020-08-04 16:12:33 -0400

    added new DFMod class version  0.0.5

--- lambdata_ivan/__init__.py
+++ lambdata_ivan/__init__.py
@@ -1,4 +1,3 @@
-
 """
 lambdata - a collection of Data Science helper functions
 """
@@ -7,24 +6,24 @@ import pandas as pd
 import numpy as np
 from lambdata_ivan.dataframe_helper import report_missing_values
 
+class DFMod():
+    def __init__(self,df):
+        self.dataFrame = df
 
-def nullReport(df):
-    df.copy
-    nullFound = df.isnull().sum().sum()
-    if (nullFound == 1):
-        answer1 = print('There is only', nullFound, 'NaN value in your DataFrame!')
-        return answer1
-    elif (nullFound > 1): 
-        answer2 = print('There are', nullFound, 'NaN values in your DataFrame.')
-        return answer2
-    else:
-        print('There are no NaN values in your DataFrame!')
+    def nullReport(self):
+        nullFound = self.dataFrame.isnull().sum().sum()
+        if (nullFound == 1):
+            answer1 = print('There is only', nullFound, 'NaN value in your DataFrame!')
+            return answer1
+        elif (nullFound > 1): 
+            answer2 = print('There are', nullFound, 'NaN values in your DataFrame.')
+            return answer2
+        else:
+            print('There are no NaN values in your DataFrame!')
 
-
-def listToColumn(df, lists,name):
-    lists.copy
-    df.copy
-    newSeries =  pd.Series(lists)
-    df[name] = newSeries
-    return df
+    def listToColumn(self, lists, name):
+        lists.copy
+        newSeries =  pd.Series(lists)
+        self.dataFrame[name] = newSeries
+        return self.dataFrame
 
