686ae8c030468b947684833076caa125d6d3e465
IvanCampos11 <39417756+IvanCampos11@users.noreply.github.com>
2020-08-04 18:04:32 -0400

    added new function to detect and clean NaN values

--- lambdata_ivan/__init__.py
+++ lambdata_ivan/__init__.py
@@ -33,4 +33,8 @@ class DFMod:
         return self.dataFrame
     
     def nullClean(self):
-
+         self.dataFrame = self.dataFrame.replace('?', np.nan).replace('nan',np.nan).replace('Nan',np.nan).replace('NaN',np.nan).replace(' ?',np.nan).replace('N/A',np.nan).replace('n/a',np.nan)
+        nullValue = self.dataFrame.isnull().sum().sum()
+        self.dataFrame.dropna(inplace=True)
+        print('There were', nullValue, "NaN's detected and removed.")
+        return self.dataFrame
