ff1609d95b9eeb4ecd5b22a378c95a909f735808
IvanCampos11 <39417756+IvanCampos11@users.noreply.github.com>
2020-08-04 16:53:34 -0400

    Update __init__.py

--- lambdata_ivan/__init__.py
+++ lambdata_ivan/__init__.py
@@ -6,10 +6,14 @@ import pandas as pd
 import numpy as np
 from lambdata_ivan.dataframe_helper import report_missing_values
 
+#Stands for DataFrameModification, This class gives you info about the df 
+# and cleans it from NaN's if you want.
 class DFMod:
     def __init__(self,df):
         self.dataFrame = df
 
+#this just gets all the null values into one number and prints how many there are.
+#this has variation to fix english if theres only one NaN or multiple as well if they're absent!
     def nullReport(self):
         nullFound = self.dataFrame.isnull().sum().sum()
         if (nullFound == 1):
@@ -21,9 +25,12 @@ class DFMod:
         else:
             print('There are no NaN values in your DataFrame!')
 
+#Give this a list and a name and it'll turn it into a series and add it as a new column.
     def listToColumn(self, lists, name):
         lists.copy
         newSeries =  pd.Series(lists)
         self.dataFrame[name] = newSeries
         return self.dataFrame
+    
+    def nullClean(self):
 
